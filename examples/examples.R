c7e5398390867587e7b74d5dc71033445488a9d2
Harvey Barnhard <harveybarnhard@gmail.com>
2020-11-22 19:37:05 -0500

    Updated examples

--- examples/examples.R
+++ examples/examples.R
@@ -1,25 +1,51 @@
+# Github example ===============================================================
+# Create some fake sinusoidal data
 set.seed(1234)
 n = 200
 X = cbind(rep(1, n),seq(1,10, length.out=n))
 y = sin(X[,2]) + rnorm(n, sd=0.5)
 H = list(matrix(2), matrix(0.1))
+
+# Perform local linear regression with three bandwidth matrices
 predvals = list()
 predvals[[1]] = linsmooth(X, y, H=H[[1]])
-predvals[[2]] = linsmooth(X, y)
-predvals[[3]] = linsmooth(X, y, H=H[[2]])
+predvals[[2]] = linsmooth(X, y, H=H[[2]])
+predvals[[3]] = linsmooth(X, y)
 
+# Perform linear regression
+predvals[[4]] = linsmooth(X, y, method="ols")
 
-png(filename="examples/looperr_example.png", width=800, height=480)
+# Plot the local linear fits using base R graphics
+png(filename="examples/looperr_example1.png", width=800, height=480)
 plot(x=X[,2], y=y,
      axes=FALSE, xaxt="n", yaxt="n", ann=FALSE,
      pch=16, cex=0.9)
-lines(X[,2], predvals[[1]]$fitted.values, col="#00CCCC", lwd=2)
-lines(X[,2], predvals[[2]]$fitted.values, col="#FFCC33", lwd=2)
-lines(X[,2], predvals[[3]]$fitted.values, col="#CC0000", lwd=2)
+lines(X[,2], predvals[[1]]$fitted.values, col="#FFCC33", lwd=2)
+lines(X[,2], predvals[[2]]$fitted.values, col="#CC0000", lwd=2)
+lines(X[,2], predvals[[3]]$fitted.values, col="#00CCCC", lwd=2)
 legend(x=0.5,y=-1,
        legend=c("Large Bandwidth: 2" ,
                 paste0("Optimal Bandwidth: ",
                        round(predvals[[2]]$bandwidth,2)),
                 "Small Bandwidth: 0.1"),
-       col=c("#00CCCC", "#FFCC33", "#CC0000"), lty=1, lwd=2, box.lty=0, bg=NA)
+       col=c("#FFCC33", "#CC0000", "#00CCCC"), lty=1, lwd=2, box.lty=0, bg=NA)
+dev.off()
+
+# Smooth the prediction errors
+predvals_meta = list()
+predvals_meta[[3]] = linsmooth(X, predvals[[3]]$loo_pred_err)
+predvals_meta[[4]] = linsmooth(X, predvals[[4]]$loo_pred_err)
+
+# Plot the prediction error points and the fitted curves
+png(filename="examples/looperr_example2.png", width=800, height=480)
+plot(x=X[,2], y=predvals[[4]]$loo_pred_err, col="#CC0000", pch=16, cex=0.9)
+points(x=X[,2], y=predvals[[3]]$loo_pred_err, col="#00CCCC", pch=16, cex=0.9)
+lines(x=X[,2], y=predvals_meta[[4]]$fitted.values, col="#CC0000", lwd=2)
+lines(x=X[,2], y=predvals_meta[[3]]$fitted.values, col="#00CCCC", lwd=2)
+
+# Add a legend
+legend(x=1,y=-1.4,
+       legend=c("Linear Regression" ,
+                "Local Linear Regression"),
+       col=c("#CC0000", "#00CCCC"), lty=1, lwd=2, box.lty=0, bg=NA)
 dev.off()
