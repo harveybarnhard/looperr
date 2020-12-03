543492504ad6e2ed13ab15bf2e49cd16f5ee92bb
Harvey Barnhard <harveybarnhard@gmail.com>
2020-12-02 21:42:11 -0500

    Update examples.R

--- examples/examples.R
+++ examples/examples.R
@@ -81,3 +81,50 @@ dt[, Zsmooth := linsmooth(X, )]
 fig = plot_ly(dt, x= ~X, y= ~Y, z= ~Znoise)
 fig = fig %>% add_markers(size=1, color=)
 add_markers(plot_ly(dt, x= ~X, y= ~Y, z= ~Znoise), size=1)
+
+# Blog post example ============================================================
+library(pBrackets)
+library(ggplot2)
+library(latex2exp)
+
+# Simulate data
+set.seed(123)
+n = 20
+X = rnorm(n, mean=0, sd=0.2)
+beta = 0.5
+Y = X*beta + rnorm(n, mean=0, sd=0.1)
+
+# Create a high-leverage point
+Y[18] = Y[18] + .5
+X[18] = X[18] + .2
+
+# Plot the regression
+ggplot(data.frame(X, Y), aes(x=X, y=Y)) +
+        geom_point() +
+        geom_smooth(method="lm", formula="y~x", se=FALSE,
+                    aes(color="With High-Leverage Point")) +
+        geom_smooth(data=data.frame(X=X[-18], Y=Y[-18]),
+                    method="lm", formula="y~x", se=FALSE,
+                    aes(x=X, y=Y,color="Without High-Leverage Point"),
+                    fullrange=TRUE) +
+        coord_cartesian(xlim=c(min(X), max(X))) +
+        geom_curve(aes(x = X[18]+0.1, y = Y[18]-0.1,
+                       xend = X[18]+0.01, yend = Y[18]-0.008),
+                   colour = "#CC0000",
+                   size=1,
+                   curvature = 0.1,
+                   arrow = arrow(length = unit(0.03, "npc"))) +
+        annotate(geom="text",x=X[18]+0.11, y=Y[18]-0.11, label="High Leverage Point",
+                 size=6) +
+        annotate(geom="text",x=-0.225, y=0.137,
+                 label=latex2exp::TeX("$\\hat{\\epsilon}_i$"),
+                 size=6, parse=TRUE) +
+        annotate(geom="text",x=-0.146 , y=0.1067,
+                 label=latex2exp::TeX("$\\hat{\\epsilon}_i^{leaveout}$"),
+                 size=6, parse=TRUE) +
+        scale_colour_manual(name="", values=c("#CC0000", "#00CCCC")) +
+        theme_void() +
+        theme(legend.position=c(0.8, 0.15),
+              legend.text=element_text(size=16))
+grid.brackets(158, 474, 158, 32)
+grid.brackets(165, 32, 165, 546)
